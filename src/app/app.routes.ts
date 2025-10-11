48670c8ed557e9ff3dc27ac5108247df20a0833a
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-10-11 23:26:56 +0200

    footer and routing to dev-web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { DruckComponent } from './druck/druck.component';
 import { ItComponent } from './it/it.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { AgbComponent } from './agb/agb.component';
+import { PolicyComponent } from './policy/policy.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
@@ -11,4 +14,7 @@ export const routes: Routes = [
     { path: 'it', component: ItComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'agb', component: AgbComponent },
+    { path: 'policy', component: PolicyComponent },
 ];
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) { }
 
   goTo3dDruck() {
     this.router.navigate(['/3d-druck']);
@@ -26,5 +27,27 @@ export class DataService {
 
   goToHome() {
     this.router.navigate(['']);
+  }
+
+  goToAGB() {
+    this.router.navigate(['/agb']);
+  }
+
+  goToImprint() {
+    this.router.navigate(['/imprint']);
+  }
+
+  goToPolicy() {
+    this.router.navigate(['/policy']);
+  }
+
+  goToWebdesign() {
+    this.router.navigate(['/webdesign']);
+  }
+
+  openExternal(url: string): void {
+    if (!isPlatformBrowser(this.platformId)) return; // SSR-Schutz
+
+    window.open(url, '_blank', 'noopener,noreferrer');
   }
 }
